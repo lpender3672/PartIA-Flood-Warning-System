475cb79b7e66aa698f0d2906c0e2158363572e7c
Jacob Trevithick <jrt66@cam.ac.uk>
2022-03-02 21:34:06 +0000

    categorisd_flood_risk function produced and demonstration program properly written

--- Task2G.py
+++ Task2G.py
@@ -1,48 +1,26 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
-from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
-from floodsystem.plot import plot_water_level_with_fit, plot_water_levels
-from floodsystem.datafetcher import fetch_measure_levels
-import matplotlib.pyplot as plt
-import matplotlib
-from floodsystem.analysis import polyfit
-import datetime
-from datetime import timedelta
-
-#stations = build_station_list()
-
-#update_water_levels(stations)
-#dt = 3
-#warned = []
-#for i in stations:
-
-    #if type(i.relative_water_level()) == float and i.relative_water_level() > 1:
-        #dates,levels = fetch_measure_levels(i.measure_id, dt=datetime.timedelta(days= dt))
-        #avgrad = (levels[0] - levels[-1])/288
-        #weeklevel = levels[0] + avgrad*6272
-        #if weeklevel > 2:
-            #warned.append([i.name,weeklevel])
-
-#print(warned)
-
-
+from floodsystem.flood import categorised_flood_risk
 
 
 stations = build_station_list()
 update_water_levels(stations)
-risk_sorted_stations = stations_level_over_threshold(stations, 0)
-
-station = risk_sorted_stations[3][0]
-
-p = 3
-dates,levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days= 1))
-poly,d0 = polyfit(dates, levels, p)
-future_T = datetime.datetime.now() - datetime.timedelta(days=1)
-future_N = matplotlib.dates.date2num(future_T)
-
-offset = matplotlib.dates.date2num(dates[0])
-level = poly(future_N - offset)
 
 
-print(station.name)
-print((level - station.typical_range[0])/(station.typical_range[1]-station.typical_range[0]))
-print(level)
+faulty,low,moderate,high,severe = categorised_flood_risk(stations)
+
+print(len(faulty))
+print(len(low))
+print(len(moderate))
+print(len(high))
+print(len(severe))
+
+print('FAULTY')
+print([i[0].name for i in faulty])
+print('LOW')
+print([i[0].name for i in low])
+print('MODERATE')
+print([i[0].name for i in moderate])
+print('HIGH')
+print([i[0].name for i in high])
+print('SEVERE')
+print([i[0].name for i in severe])
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,4 +1,8 @@
 
+from floodsystem.datafetcher import fetch_measure_levels
+import matplotlib
+from floodsystem.analysis import polyfit
+import datetime
 
 
 def stations_level_over_threshold(stations, tol):
@@ -19,3 +23,44 @@ def stations_highest_rel_level(stations, N):
 
     return stations_level_over_threshold(stations, 0)[0:N]
         
+
+def categorised_flood_risk(stations):
+    low = stations_level_over_threshold(stations,0.8)
+    moderate = stations_level_over_threshold(stations, 1)
+    high = []
+    severe = []
+    faulty = []
+
+    dt = 1
+    for i in range(0, len(moderate)-1):
+        dates,levels = fetch_measure_levels(moderate[i][0].measure_id, dt=datetime.timedelta(days=dt))
+        try:
+            A = levels[-1]
+            B = levels[0]
+            if B - A > 0.3:
+                if moderate[i][0].typical_range_consistent():
+                    p = 3
+                    poly,d0 = polyfit(dates, levels, p)
+                    future_T = datetime.datetime.now() - datetime.timedelta(days=2)
+                    future_N = matplotlib.dates.date2num(future_T)
+                    offset = matplotlib.dates.date2num(dates[0])
+                    level = poly(future_N - offset)
+                    rel_level = (level - moderate[i][0].typical_range[0])/(moderate[i][0].typical_range[1]-moderate[i][0].typical_range[0])
+                    if rel_level > 1.5:
+                        severe.append(moderate[i])
+                    else:
+                        high.append(moderate[i])
+                else:
+                    high.append(moderate[i])
+
+
+        except IndexError:
+            faulty.append(moderate[i])
+
+    print(len(low))
+    low = [x for x in low if x not in moderate]
+    moderate  = [x for x in moderate if x not in faulty]
+    moderate  = [x for x in moderate if x not in high]
+    moderate  = [x for x in moderate if x not in severe]
+    
+    return(faulty, low, moderate, high, severe)
