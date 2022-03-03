bf75044988318fbca8f31fd41d8625474a34e85a
Jacob Trevithick <jrt66@cam.ac.uk>
2022-03-03 09:40:14 +0000

    fixed issue with Task 2E demonstration

--- Task2E.py
+++ Task2E.py
@@ -8,21 +8,27 @@ import datetime
 def run():
 
     dt = 5 # past 5 days
+    error_counter = 0
 
 
     # Build list of stations
     stations = build_station_list()
     update_water_levels(stations)
 
-    most_at_risk = stations_highest_rel_level(stations, 5) # 5 most at risk stations
+    most_at_risk = stations_highest_rel_level(stations, 6) #6 stations with highest level accounting for faulty stations
 
-    for s,r in most_at_risk:
-        
-        dates, levels = fetch_measure_levels(s.measure_id, dt=datetime.timedelta(days=dt))
-
-        print(most_at_risk)
-        plot_water_levels(s, dates, levels)
-
+    counter = 0
+    while counter < 5:
+        for s,r in most_at_risk:
+            dates, levels = fetch_measure_levels(s.measure_id, dt=datetime.timedelta(days=dt))
+            try:
+                dates[-1]
+                print(most_at_risk)
+                plot_water_levels(s, dates, levels)
+                counter += 1
+            except:
+                pass
+    
 
 
 if __name__ == "__main__":
